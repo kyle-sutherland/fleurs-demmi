711dbe680296fb897ceda42120ee3fd4df74fd35
kyle-sutherland <velveteen.rever@gmail.com>
2026-04-23 03:02:49 -0400

    hardened image source whitelist

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,17 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: 'https', hostname: 'items.squareup.com' },
-      { protocol: 'https', hostname: 'items.squareupsandbox.com' },
-      { protocol: 'https', hostname: 'square-web-production-f.squarecdn.com' },
-      { protocol: 'https', hostname: '*.amazonaws.com' },
+      { protocol: "https", hostname: "items.squareup.com" },
+      { protocol: "https", hostname: "items.squareupsandbox.com" },
+      { protocol: "https", hostname: "square-web-production-f.squarecdn.com" },
+      {
+        protocol: "https",
+        hostname: "items-images-sandbox.s3.us-west-2.amazonaws.com",
+      },
+      {
+        protocol: "https",
+        hostname: "items-images-production.s3.us-west-2.amazonaws.com",
+      },
     ],
   },
 };
